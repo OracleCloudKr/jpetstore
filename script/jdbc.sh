363e0f96ff98ac0c76a752f01b10494f88f3f2b3
JongGyou Kim <jonggyou.kim@oracle.com>
2017-07-18 18:03:36 +0900

    update script

--- script/jdbc.sh
+++ script/jdbc.sh
@@ -2,4 +2,5 @@
 
 scp -i ./privateKey script/create-jdbc.* opc@140.86.1.66:/tmp/
 ssh -i ./privateKey opc@140.86.1.66 "chmod +x /tmp/create-jdbc.sh"
+ssh -i ./privateKey opc@140.86.1.66 "/tmp/create-jdbc.sh"
 
