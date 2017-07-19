d904db45328d2c57f7dd1e85c012b7005b1c6e5a
JongGyou Kim <jonggyou.kim@oracle.com>
2017-07-19 14:02:40 +0900

    update jdbc.sh

--- script/jdbc.sh
+++ script/jdbc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-scp -i ./privateKey script/create-jdbc.* opc@140.86.1.66:/tmp/
-ssh -i ./privateKey opc@140.86.1.66 "chmod +x /tmp/create-jdbc.sh"
-ssh -i ./privateKey opc@140.86.1.66 "/tmp/create-jdbc.sh"
+scp -o StrictHostKeyChecking=no -i ./privateKey script/create-jdbc.* opc@140.86.1.66:/tmp/
+ssh -o StrictHostKeyChecking=no -i ./privateKey opc@140.86.1.66 "chmod +x /tmp/create-jdbc.sh"
+ssh -o StrictHostKeyChecking=no -i ./privateKey opc@140.86.1.66 "/tmp/create-jdbc.sh"
 
